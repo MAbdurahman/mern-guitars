4f2e462098b3ed7d0f70277e276b8476f1ba656d
MAbdurahman <mdbdrrhm@gmail.com>
2021-06-24 07:31:54 -0500

    Configure passport middleware portion

--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ const colors = require('colors');
 const morgan = require('morgan');
 const xss = require('xss-clean');
 const mongoSanitize = require('express-mongo-sanitize');
+const passport = require('passport');
+const { jwtStrategy } = require('./middleware/passport');
 
 const routes = require('./routes/routeIndex');
 const connectDatabase = require('./config/databaseConfig');
@@ -27,6 +29,8 @@ if (process.env.NODE_ENV === 'DEVELOPMENT') {
 app.use(express.json());
 app.use(xss());
 app.use(mongoSanitize());
+app.use(passport.initialize());
+passport.use('jwt', jwtStrategy);
 
 //**************** app routes ****************//
 app.use('/api/v1.0', routes);
