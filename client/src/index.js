c3221abfb5f6f5658eb0b33d2b816d651c6c6b5b
MAbdurahman <mdbdrrhm@gmail.com>
2021-07-04 05:02:23 -0500

    Move app.js to directory app

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
-import App from './app';
+import App from './app/app';
 import './styles/styles.css';
 
 ReactDOM.render(
